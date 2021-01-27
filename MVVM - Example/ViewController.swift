57786ad2746718187d6a578e28102c12286369c2
Justyna <kowalkowska.just@gmail.com>
2021-01-27 18:53:04 +0100

    Configuring Table View.

--- MVVM - Example/ViewController.swift
+++ MVVM - Example/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //  MARK: - Properties
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureTableView()
     }
 
+    //MARK: - Helper functions
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        view.addSubview(tableView)
+        tableView.anchor(top: view.topAnchor, left: view.leftAnchor,
+                         bottom: view.bottomAnchor, right: view.rightAnchor,
+                         paddingTop: 5, paddingLeft: 5,
+                         paddingBottom: 5, paddingRight: 5)
+    }
+}
 
+//MARK: - Extension UITableViewDataSource and UITableViewDelegate
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Justyna programuje"
+        return cell
+    }
 }
 
